beba07a284345a1178abba029b2ab2f3e9531013
galalkoro <koro-galal@outlook.com>
2022-07-01 23:30:29 +0200

    post class aded added proberties, constructor for all proberties & getters

--- models/Post.php
+++ models/Post.php
@@ -1,1 +1,49 @@
 <?php
+class Post{
+    private $title;
+    private $date;
+    private $Content;
+    private $author;
+
+
+    /**
+     * @param $title
+     * @param $date
+     * @param $Content
+     * @param $author
+     */
+    public function __construct($title, $date, $Content, $author)
+    {
+        $this->title = $title;
+        $this->date = $date;
+        $this->Content = $Content;
+        $this->author = $author;
+    }
+
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+
+    public function getContent()
+    {
+        return $this->Content;
+    }
+
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+
+
+}
