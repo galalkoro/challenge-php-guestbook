a5a016eb731c0d04bfb1280aa206c6517f98aad1
galalkoro <koro-galal@outlook.com>
2022-07-01 23:34:39 +0200

    added index html php this will include most html files to be separate between index php

--- index.html.php
+++ index.html.php
@@ -1,1 +1,24 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css">
+
+    <title>PHP Guestbook</title>
+</head>
+<body>
+<div class="container">
+    <form method="post"  action="index.php">
+        <input type="text" name="author" placeholder="Author">
+        <input type="text" name="title" placeholder="Title">
+        <input type="text" name="content" placeholder="Message">
+        <button class="saver">Save</button>
+    </form>
+</div>
+
+
+</body>
+</html>
