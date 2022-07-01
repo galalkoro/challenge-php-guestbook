e591d7073b52355516611ee5a72e949f5d1c67e3
galalkoro <koro-galal@outlook.com>
2022-07-01 23:33:03 +0200

    added index php to require files this file will be used onley for require files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+session_start();
+
+require 'models/Post.php';
+require 'models/PostLoader.php';
+require 'DataCenter/data.txt';
+require 'index.html.php';
+
+
+
+
+
+
+
+
+
+
+
+
